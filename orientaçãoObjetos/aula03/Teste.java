611f77edf90c8f6459afb6e2018068e3d46ec21d
Silvano AP <silvanoeng@hotmail.com>
2021-10-09 19:19:47 -0300

    Aulas extra do 1°modulo +praTi

---
+++ orientaçãoObjetos/aula03/Teste.java
@@ -0,0 +1,15 @@
+
+
+public class Teste
+{
+    public static void main(String args[]){
+        System.out.println(Math.E);
+        
+        System.out.println(Math.PI);
+        
+        String texto = "Testando uma String.";
+        
+        System.out.println(texto.toUpperCase());
+        
+    }
+}
