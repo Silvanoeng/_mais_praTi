f21cfbb897de2e84ea76f633db016fbdc2ac5aa4
Silvano AP <silvanoeng@hotmail.com>
2021-10-11 21:11:22 -0300

    Criando classe Funcionaio

---
+++ orientaçãoObjetos/aula04/src/revisao_heranca/Funcionario.java
@@ -0,0 +1,55 @@
+package revisao_heranca;
+
+public class Funcionario {
+    private String nome;
+    private String cpf;
+    private double salario;
+
+    public Funcionario(){};
+
+    public Funcionario(String nome, String cpf, double salario) {
+        this.nome = nome;
+        this.cpf = cpf;
+        this.salario = salario;
+    }
+
+    public double getBonificacao() {
+        return this.salario * 0.1;
+    }
+
+    public String getNome() {
+        return nome;
+    }
+
+    public void setNome(String nome) {
+        if (nome.equals(null) || nome.equals("")) {
+            System.out.println("Esse valor não é valido.");
+            return;
+        }
+        this.nome = nome;
+    }
+
+    public String getCpf() {
+        return cpf;
+    }
+
+    public void setCpf(String cpf) {
+        if (cpf.equals(null) || cpf.equals("")) {
+            System.out.println("Esse valor não é valido.");
+            return;
+        }
+        this.cpf = cpf;
+    }
+
+    public double getSalario() {
+        return salario;
+    }
+
+    public void setSalario(double salario) {
+        if (salario<0) {
+            System.out.println("Esse valor não é valido.");
+            return;
+        }
+        this.salario = salario;
+    }
+}
---
+++ orientaçãoObjetos/aula04/src/revisao_heranca/TesteFuncionario.java
@@ -0,0 +1,18 @@
+package revisao_heranca;
+
+public class TesteFuncionario {
+    public static void main(String[] args) {
+        Funcionario funcionario01 = new Funcionario("Silvano", "00101110011", 3000.00);
+
+        Funcionario funcionario02 = new Funcionario();
+        funcionario02.setNome("Matheus");
+        funcionario02.setCpf("00202220022");
+        funcionario02.setSalario(4500.00);
+
+        System.out.println(funcionario01.getNome());
+        System.out.println(funcionario01.getBonificacao());
+        System.out.println(funcionario02.getNome());
+        System.out.println(funcionario02.getBonificacao());
+
+    }
+}
