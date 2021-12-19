e3e34637b81eaef5af84ecfc78a48c5eb5e949a9
Silvano AP <silvanoeng@hotmail.com>
2021-12-18 22:56:51 -0300

    praticando

--- untitled/src/Numero.java
+++ untitled/src/Numero.java
@@ -1,5 +1,17 @@
-package PACKAGE_NAME;public class Numero {
+public class Numero {
     public static void main(String[] args) {
+        int num = 1;
+        if (num>1 | num!=1){
+            System.out.println("Teste.");
 
+        }else {
+            System.out.println(saudacao());
+        }
+
+    }
+
+    public static String saudacao(){
+
+        return "Olá mundo!";
     }
 }
